326b43d48322d7f7602b58bc9fc6a3a038fbf743
Joey Driscoll <driscoll.recording@gmail.com>
2019-07-10 09:01:42 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace family_dictionary
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+      Dictionary<string, Dictionary<string, string>> family = new Dictionary<string, Dictionary<string, string>>();
+      family.Add("father", new Dictionary<string, string>(){
+          {"name", "Dan"},
+          {"age", "63"}
+      });
+      family.Add("mother", new Dictionary<string, string>(){
+          {"name", "Sue"},
+          {"age", "59"}
+      });
+      family.Add("brother", new Dictionary<string, string>(){
+          {"name", "Sam"},
+          {"age", "21"}
+      });
+
+
+      foreach (KeyValuePair<string, Dictionary<string, string>> familyMember in family)
+      {
+        Console.WriteLine($" {familyMember.Value["name"]} is my {familyMember.Key} and is {familyMember.Value["age"]} years old");
+      }
+
+
+      // Remember that you can use square bracket notation to get to the value of a key, and that a dictionary has a Key and a Value property.
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
+
+
